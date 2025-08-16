50eb69cf86974f4e79cb3b3b8def6aa3944f7c5b
Akshay Ambekar <akshay.ambekar@dominodatalab.com>
2025-08-15 21:24:39 -0700

    Pushed from Domino: https://metlife.cs.domino.tech/workspace/ddl-akshay/Fraud-Detection-Workshop?executionId=689e5411a9755e62313445b5

--- exercises/d_TrainingAndEvaluation/workflow.py
+++ exercises/d_TrainingAndEvaluation/workflow.py
@@ -2,52 +2,65 @@
 import os
 from flytekit import workflow, task
 from flytekitplugins.domino.task import DominoJobConfig, DominoJobTask
-from flytekitplugins.domino.task import DatasetSnapshot  
+from flytekitplugins.domino.task import DatasetSnapshot
 
+DOMINO_WORKING_DIR = os.environ["DOMINO_WORKING_DIR"]
+# Command to run this code
+# pyflyte run --remote exercises/d_TrainingAndEvaluation/workflow.py credit_card_fraud_detection_workflow
 
 @workflow
 def credit_card_fraud_detection_workflow():
-    transformed_filename = 'transformed_cc_transactions.csv'
+    transformed_filename = "transformed_cc_transactions.csv"
 
     ada_training_task = DominoJobTask(
-        name='Train AdaBoost classifier',
-        domino_job_config=DominoJobConfig(Command="python exercises/d_TrainingAndEvaluation/trainer_ada.py"),
-        inputs={'transformed_filename': str},
-        outputs={'results': str},
+        name="Train AdaBoost classifier",
+        domino_job_config=DominoJobConfig(
+            Command=f"python {DOMINO_WORKING_DIR}/exercises/d_TrainingAndEvaluation/trainer_ada.py"
+        ),
+        inputs={"transformed_filename": str},
+        outputs={"results": str},
         use_latest=True,
-        cache=True
+        cache=True,
     )
 
     gnb_training_task = DominoJobTask(
-        name='Train GaussianNB classifier',
-        domino_job_config=DominoJobConfig(Command="python exercises/d_TrainingAndEvaluation/trainer_gnb.py"),
-        inputs={'transformed_filename': str},
-        outputs={'results': str},
+        name="Train GaussianNB classifier",
+        domino_job_config=DominoJobConfig(
+            Command=f"python {DOMINO_WORKING_DIR}/exercises/d_TrainingAndEvaluation/trainer_gnb.py"
+        ),
+        inputs={"transformed_filename": str},
+        outputs={"results": str},
         use_latest=True,
-        cache=True
+        cache=True,
     )
-    
+
     xgb_training_task = DominoJobTask(
-        name='Train XGBoost classifier',
-        domino_job_config=DominoJobConfig(Command="python exercises/d_TrainingAndEvaluation/trainer_xgb.py"),
-        inputs={'transformed_filename': str},
-        outputs={'results': str},
+        name="Train XGBoost classifier",
+        domino_job_config=DominoJobConfig(
+            Command=f"python {DOMINO_WORKING_DIR}/exercises/d_TrainingAndEvaluation/trainer_xgb.py"
+        ),
+        inputs={"transformed_filename": str},
+        outputs={"results": str},
         use_latest=True,
-        cache=True
+        cache=True,
     )
 
     ada_results = ada_training_task(transformed_filename=transformed_filename)
     gnb_results = gnb_training_task(transformed_filename=transformed_filename)
     xgb_results = xgb_training_task(transformed_filename=transformed_filename)
-    
+
     compare_task = DominoJobTask(
-        name='Compare training results',
-        domino_job_config=DominoJobConfig(Command="python exercises/d_TrainingAndEvaluation/compare.py"),
-        inputs={'ada_results': str, 'gnb_results': str, 'xgb_results': str},
-        outputs={'consolidated': str}, 
-        use_latest=True
+        name="Compare training results",
+        domino_job_config=DominoJobConfig(
+            Command=f"python {DOMINO_WORKING_DIR}/exercises/d_TrainingAndEvaluation/compare.py"
+        ),
+        inputs={"ada_results": str, "gnb_results": str, "xgb_results": str},
+        outputs={"consolidated": str},
+        use_latest=True,
     )
-    
-    comparison = compare_task(ada_results=ada_results, gnb_results=gnb_results, xgb_results=xgb_results)
+
+    comparison = compare_task(
+        ada_results=ada_results, gnb_results=gnb_results, xgb_results=xgb_results
+    )
 
     return comparison
